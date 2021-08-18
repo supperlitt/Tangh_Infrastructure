f1e88682665f9843e89a926c6c7444bcc8248551
超级_小小 <wojiaotanghao@qq.com>
2021-08-18 10:08:58 +0800

    1
    1

--- SupperlittTool/File/ExcelHelper.cs
+++ SupperlittTool/File/ExcelHelper.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.IO;
 using NPOI.XSSF.UserModel;
 using NPOI.HSSF.UserModel;
+using NPOI.XSSF.Streaming;
 
 namespace SupperlittTool
 {
@@ -31,6 +32,103 @@ namespace SupperlittTool
         }
 
         /// <summary>
+        /// 对已有的Excel追加数据，从指定行开始
+        /// </summary>
+        /// <param name="data">要导入的数据</param>
+        /// <param name="isColumnWritten">DataTable的列名是否要导入</param>
+        /// <param name="sheetName">要导入的excel的sheet的名称</param>
+        /// <param name="startIndex">指定追加行号</param>
+        /// <returns>导入数据行数(包含列名那一行)</returns>
+        public int DataTableToExcel(DataTable data, string sheetName, bool isColumnWritten, int startIndex)
+        {
+            int i = 0;
+            int j = 0;
+            int count = 0;
+            ISheet sheet = null;
+            using (fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                if (fileName.IndexOf(".xlsx") > 0) // 2007版本
+                    workbook = new XSSFWorkbook(fs);
+                else if (fileName.IndexOf(".xls") > 0) // 2003版本
+                    workbook = new HSSFWorkbook(fs);
+            }
+
+            try
+            {
+                if (workbook != null)
+                {
+                    if (workbook.NumberOfSheets > 0)
+                    {
+                        sheet = workbook.GetSheetAt(0);
+                    }
+                    else
+                    {
+                        sheet = workbook.CreateSheet(sheetName);
+                    }
+                }
+                else
+                {
+                    return -1;
+                }
+
+                if (isColumnWritten == true) //写入DataTable的列名
+                {
+                    IRow row = sheet.CreateRow(0);
+                    for (j = 0; j < data.Columns.Count; ++j)
+                    {
+                        row.CreateCell(j).SetCellValue(data.Columns[j].ColumnName);
+                    }
+
+                    count = 1 + startIndex;
+                }
+                else
+                {
+                    count = 0 + startIndex;
+                }
+
+                for (i = 0; i < data.Rows.Count; ++i)
+                {
+                    IRow row = sheet.CreateRow(count);
+                    for (j = 0; j < data.Columns.Count; ++j)
+                    {
+                        if (data.Rows[i][j] == DBNull.Value || data.Rows[i][j] == null)
+                        {
+                            row.CreateCell(j).SetCellValue("");
+                        }
+                        else
+                        {
+                            if (data.Columns[j].DataType == typeof(double) || data.Columns[j].DataType == typeof(float))
+                            {
+                                row.CreateCell(j).SetCellValue(Convert.ToDouble(data.Rows[i][j]));
+                            }
+                            else
+                            {
+                                row.CreateCell(j).SetCellValue(data.Rows[i][j].ToString());
+                            }
+                        }
+                    }
+
+                    ++count;
+                }
+
+                sheet.ForceFormulaRecalculation = true;
+
+                //将内存数据写到文件
+                using (fs = new FileStream(fileName, FileMode.Open, FileAccess.Write))
+                {
+                    workbook.Write(fs);
+                    workbook.Close();
+                }
+
+                return count;
+            }
+            catch (Exception ex)
+            {
+                return -1;
+            }
+        }
+
+        /// <summary>
         /// 将DataTable数据导入到excel中
         /// </summary>
         /// <param name="data">要导入的数据</param>
@@ -46,9 +144,9 @@ namespace SupperlittTool
 
             fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
             if (fileName.IndexOf(".xlsx") > 0) // 2007版本
-                workbook = new XSSFWorkbook();
+                workbook = new XSSFWorkbook(fs);
             else if (fileName.IndexOf(".xls") > 0) // 2003版本
-                workbook = new HSSFWorkbook();
+                workbook = new HSSFWorkbook(fs);
 
             try
             {
@@ -85,13 +183,20 @@ namespace SupperlittTool
                     ++count;
                 }
 
-                workbook.Write(fs); //写入到excel
+                //workbook.Write(fs); //写入到excel
                 return count;
             }
             catch (Exception ex)
             {
                 Console.WriteLine("Exception: " + ex.Message);
                 return -1;
+            }
+            finally
+            {
+                if (workbook != null)
+                {
+                    workbook.Close();
+                }
             }
         }
 
@@ -166,6 +271,155 @@ namespace SupperlittTool
             {
                 Console.WriteLine("Exception: " + ex.Message);
                 return null;
+            }
+        }
+
+        /// <summary>
+        /// 大文件导出
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="sheetName"></param>
+        /// <param name="isColumnWritten"></param>
+        /// <param name="startIndex"></param>
+        /// <returns></returns>
+        public int DataTableToExcel_SXSS(DataTable data, string sheetName, bool isColumnWritten, int startIndex)
+        {
+            //创建SXSSFWorkbook对象
+            SXSSFWorkbook wb = new SXSSFWorkbook();
+
+            //创建sheet　并指定sheet的名字
+            ISheet sheet1 = wb.CreateSheet(sheetName);
+
+            if (startIndex == 0)
+            {
+                //创建第一行
+                IRow row0 = sheet1.CreateRow(startIndex);
+                for (int index = 0; index < data.Columns.Count; index++)
+                {
+                    var dc = data.Columns[index];
+                    row0.CreateCell(index).SetCellValue(dc.ColumnName);
+                }
+
+                startIndex++;
+            }
+
+            // 构建大数据量
+            for (int i = 0; i < data.Rows.Count; i++)
+            {
+                DataRow dr = data.Rows[i];
+                IRow rowi = sheet1.CreateRow(startIndex);
+                for (int index = 0; index < data.Columns.Count; index++)
+                {
+                    var dc = data.Columns[index];
+
+                    if (dr[index] == null || dr[index] == DBNull.Value)
+                    {
+                        rowi.CreateCell(index).SetCellValue("");
+                    }
+                    else
+                    {
+                        if (dc.DataType == typeof(double) || dc.DataType == typeof(float))
+                        {
+                            rowi.CreateCell(index).SetCellValue(Convert.ToDouble(dr[index]));
+                        }
+                        else
+                        {
+                            rowi.CreateCell(index).SetCellValue(dr[index].ToString());
+                        }
+                    }
+                }
+
+                startIndex++;
+            }
+
+            try
+            {
+                //将内存中的数据写入磁盘
+                using (FileStream filestream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    wb.Write(filestream);
+                    filestream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
+            }
+
+            return startIndex;
+        }
+
+        /// <summary>
+        /// 大文件导出,多sheet
+        /// </summary>
+        /// <param name="dataList"></param>
+        /// <param name="sheetName"></param>
+        /// <param name="isColumnWritten"></param>
+        public void DataTableToExcel_SXSS(List<DataTable> dataList, string sheetName, bool isColumnWritten)
+        {
+            //创建SXSSFWorkbook对象
+            SXSSFWorkbook wb = new SXSSFWorkbook();
+
+            // 构建大数据量
+            for (int k = 0; k < dataList.Count; k++)
+            {
+                var data = dataList[k];
+
+                // 创建sheet　并指定sheet的名字
+                ISheet sheet1 = wb.CreateSheet(sheetName + k);
+
+                //创建第一行
+                IRow row0 = sheet1.CreateRow(0);
+                for (int index = 0; index < data.Columns.Count; index++)
+                {
+                    var dc = dataList[0].Columns[index];
+                    row0.CreateCell(index).SetCellValue(dc.ColumnName);
+                }
+
+                for (int i = 0; i < data.Rows.Count; i++)
+                {
+                    if (i % 1000 == 0)
+                    {
+                        Console.Write(k + "-" + i + " ");
+                    }
+
+                    DataRow dr = data.Rows[i];
+                    IRow rowi = sheet1.CreateRow(i + 1);
+                    for (int index = 0; index < data.Columns.Count; index++)
+                    {
+                        var dc = data.Columns[index];
+
+                        if (dr[index] == null || dr[index] == DBNull.Value)
+                        {
+                            rowi.CreateCell(index).SetCellValue("");
+                        }
+                        else
+                        {
+                            if (dc.DataType == typeof(double) || dc.DataType == typeof(float))
+                            {
+                                rowi.CreateCell(index).SetCellValue(Convert.ToDouble(dr[index]));
+                            }
+                            else
+                            {
+                                rowi.CreateCell(index).SetCellValue(dr[index].ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
+            try
+            {
+                //将内存中的数据写入磁盘
+                using (FileStream filestream = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    wb.Write(filestream);
+                    filestream.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.ToString());
             }
         }
 
---
+++ Tangh.NewInfrastructure/Http/HttpClientHelper2.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tangh.NewInfrastructure
+{
+    public class HttpClientHelper2
+    {
+        private static Dictionary<string, string> headers = null;
+
+        public static void InitHeader(Dictionary<string, string> headers)
+        {
+            HttpClientHelper2.headers = headers;
+        }
+
+        /// <summary>
+        /// get请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string GetResponse(string url)
+        {
+            if (url.StartsWith("https"))
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            //HttpClientHandler handler = new HttpClientHandler() { AllowAutoRedirect = false };
+            //handler.ServerCertificateCustomValidationCallback = (message, cert, chain, error) => true;
+            var handler = new HttpClientHandler();
+            
+            // 高版本才有.net core
+            // var handler = new HttpClientHandler { ServerCertificateCustomValidationCallback = (message, certificate2, arg3, arg4) => true };
+            HttpClient httpClient = new HttpClient(handler);
+            if (headers != null && headers.Count > 0)
+            {
+                foreach (var item in headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(item.Key, item.Value);
+                }
+            }
+
+            string result = string.Empty;
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode || response.StatusCode == HttpStatusCode.Redirect)
+            {
+                result = response.Content.ReadAsStringAsync().Result;
+            }
+
+            if (string.IsNullOrEmpty(result))
+            {
+
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// get请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static byte[] GetResponse_Bytes(string url)
+        {
+            if (url.StartsWith("https"))
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            HttpClientHandler handler = new HttpClientHandler() { AllowAutoRedirect = false };
+
+            // 高版本才有.net core
+            // handler.ServerCertificateCustomValidationCallback = (message, cert, chain, error) => true;
+
+            HttpClient httpClient = new HttpClient(handler);
+            if (headers != null && headers.Count > 0)
+            {
+                foreach (var item in headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(item.Key, item.Value);
+                }
+            }
+
+            byte[] result = new byte[0];
+            HttpResponseMessage response = httpClient.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode || response.StatusCode == HttpStatusCode.Redirect)
+            {
+                result = response.Content.ReadAsByteArrayAsync().Result;
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// post请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="postData">post数据</param>
+        /// <returns></returns>
+        public static string PostResponse(string url, string postData)
+        {
+            if (url.StartsWith("https"))
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            HttpClientHandler handler = new HttpClientHandler() { AllowAutoRedirect = false };
+
+            // 高版本才有.net core
+            // handler.ServerCertificateCustomValidationCallback = (message, cert, chain, error) => true;
+            HttpContent httpContent = new StringContent(postData);
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+            HttpClient httpClient = new HttpClient(handler);
+            HttpResponseMessage response = httpClient.PostAsync(url, httpContent).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// post请求
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="postData">post数据</param>
+        /// <returns></returns>
+        public static string PostResponse_JSON(string url, string postData)
+        {
+            if (url.StartsWith("https"))
+                System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
+            HttpClientHandler handler = new HttpClientHandler() { AllowAutoRedirect = false };
+            // 高版本才有.net core
+            // handler.ServerCertificateCustomValidationCallback = (message, cert, chain, error) => true;
+
+            HttpContent httpContent = new StringContent(postData);
+            httpContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpClient httpClient = new HttpClient(handler);
+            HttpResponseMessage response = httpClient.PostAsync(url, httpContent).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string result = response.Content.ReadAsStringAsync().Result;
+                return result;
+            }
+
+            return string.Empty;
+        }
+    }
+}
